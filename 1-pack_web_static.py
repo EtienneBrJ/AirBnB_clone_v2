7c48b1f3cd4cffe5ffc458e844d1761b748374e3
EtienneBrJ <etiennebrxv@gmail.com>
2021-08-26 15:30:34 +0300

    Compress web_static into versions

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,7 +10,7 @@ def do_pack():
     """Compress files to /version"""
     local("mkdir -p versions")
     archive = local("tar -cvzf versions/web_static_{}.tgz web_static"
-                   .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
-                   capture=True)
+                    .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
+                    capture=True)
 
     return archive if archive else None
