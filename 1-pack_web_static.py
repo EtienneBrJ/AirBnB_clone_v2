612959244412a4870b762e929a0c1a41312f65bd
EtienneBrJ <etiennebrxv@gmail.com>
2021-08-26 15:29:47 +0300

    Compress web_static into versions

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Fabric script that generates a .tgz archive
+    from the contents of the web_static
+"""
+from fabric.operations import local
+from datetime import datetime
+
+
+def do_pack():
+    """Compress files to /version"""
+    local("mkdir -p versions")
+    archive = local("tar -cvzf versions/web_static_{}.tgz web_static"
+                   .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
+                   capture=True)
+
+    return archive if archive else None
