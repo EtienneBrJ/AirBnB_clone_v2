67fe561f4adc681b60eded3e1f342a5c53b845a3
EtienneBrJ <etiennebrxv@gmail.com>
2021-08-28 03:18:11 +0300

    Fabric prompt password - Error

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,28 +3,24 @@
     that distributes an archive to your web servers,
     using the function do_deploy
 """
-from fabric.api import run, put
-from fabric.api import env
-from os import path
+from getpass import getpass
+from fabric.api import run, put, env
+import os.path
 
 
 env.hosts = ['34.75.72.178', '34.139.192.61']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/holberton'
 
 
 def do_deploy(archive_path):
     """Distributes an archive to a web server
     """
-    if path.isfile(archive_path) is False:
+    #env.password = getpass('Enter the password for %s: ' % env.hosts[0])
+    if os.path.isfile(archive_path) is False:
         return False
     file = archive_path.split("/")[-1]
     name = file.split(".")[0]
 
     if put(archive_path, "/tmp/{}".format(file)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).succeeded is False:
         return False
     if run("mkdir -p /data/web_static/releases/{}/".
            format(name)).succeeded is False:
@@ -39,6 +35,9 @@ def do_deploy(archive_path):
            .format(name, name)).succeeded is False:
         return False
     if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).succeeded is False:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/".
            format(name)).succeeded is False:
         return False
     if run("rm -rf /data/web_static/current").failed is True:
